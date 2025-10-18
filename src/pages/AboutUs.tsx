4238dccff02e1a728fa43487728db567f8427893
MelmaaTech_Eluru <128880871+melmaatechelr@users.noreply.github.com>
2025-10-18 22:27:22 +0530

    Updated AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -108,7 +108,7 @@ The Campus at Vatluru is a place where students from all walks of life are under
  </li>
                   <li className="flex items-start">
                     <span className="text-green-600 mr-2">•</span>
-                    Develop ethical, responsible, and globally competent engineers
+To Develop Compitent  Engineering Professionals Through High Quality Acadamic Instrucions And  Industry  Collaborations  ,with A Focus On Sustainable And Inclusive Technology.
                   </li>
                   <li className="flex items-start">
                     <span className="text-green-600 mr-2">•</span>
